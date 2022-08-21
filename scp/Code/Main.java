35a864f7f2b758e0f99e0d07eb823d791e76e893
Papyoz <111503973+Papyoz@users.noreply.github.com>
2022-08-21 23:35:11 +0300

    Update Main.java

--- scp/Code/Main.java
+++ scp/Code/Main.java
@@ -1,2 +1,30 @@
-package Code;public class Main {
+package Code;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double X = input.nextDouble();
+        double Y = input.nextDouble();
+        Circle c1 = new Circle(X,Y);
+        System.out.println("x = " + c1.x + "\ny = " + c1.y + "\nLenght without args = " + c1.lenght() + "\nLenght with args = " + c1.lenght(32, 19));
+    }
+}
+class Circle{
+    double x, y;
+    {
+        double pi = Math.PI;
+        x = 1; y = 1;
+    }
+    Circle(){}
+    Circle(double x, double y){
+        this.x = x; this.y = y;
+    }
+    double lenght(double x, double y){
+        return Math.sqrt(x*x + y*y);
+    }
+    double lenght(){
+        return Math.sqrt(x*x + y*y);
+    }
 }
